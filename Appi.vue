d635deee5a1477aa2c6fb360915f8083dfddcb2b
Volodymyr77 <62813857+Volodymyr77@users.noreply.github.com>
2020-08-26 17:38:36 +0300

    Add files via upload

---
+++ Appi.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" v-model="text" />
+    <ul>
+      <li v-for="(item, i) in arr" :key="i">
+        <span>{{item}}</span>
+      </li>
+    </ul>
+    <button @click="add" v-if="arr.length <=10">add todo</button>
+    <button @click="remove(i)">delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      text: '',
+      arr: [],
+    };
+  },
+  methods: {
+    add() {
+      this.arr.push(this.text);
+      this.text = '';
+    },
+
+    remove(i) {
+      this.arr.splice(i, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
